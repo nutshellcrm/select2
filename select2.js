6b1d2db1ce6e835f6f0e015d8070ca5f4f818089
Dylan Goings <dylan@dgoings.com>
2013-07-24 18:31:50 -0400

    add times symbol to select2 tag close

--- select2.js
+++ select2.js
@@ -2624,7 +2624,7 @@ the specific language governing permissions and limitations under the Apache Lic
                 enabledItem = $(
                     "<li class='select2-search-choice'>" +
                     "    <div></div>" +
-                    "    <a href='#' onclick='return false;' class='select2-search-choice-close' tabindex='-1'></a>" +
+                    "    <a href='#' onclick='return false;' class='select2-search-choice-close' tabindex='-1'>&times;</a>" +
                     "</li>"),
                 disabledItem = $(
                     "<li class='select2-search-choice select2-locked'>" +
